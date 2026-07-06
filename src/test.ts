0de2207b6288944f2ea6aa7561effa41055a82e5
komallsingh <singhkomal11711@gmail.com>
2026-07-06 14:00:49 +0530

    connected: PostgreSql database

---
+++ src/test.ts
@@ -0,0 +1,15 @@
+import {pool} from "./config/db"
+
+async function test() {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    console.log("Connected!");
+    console.log(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await pool.end();
+  }
+}
+
+test();
