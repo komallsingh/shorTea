e4fd15703d90fe807cdaed7f45467eca5614e6ce
komallsingh <singhkomal11711@gmail.com>
2026-07-20 13:13:59 +0530

    feat: add Helmet security headers and Morgan request logging

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,20 @@ import { errorHandler } from "./middleware/errorHandler";
 import authRoutes from "./routes/auth.routes";
 import cors from "cors";
 import { rateLimiter } from "./middleware/rateLimiter";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app=express();
 app.use(cors());
+app.use(helmet());
+//app.use(morgan("dev"));
+app.use(
+    morgan(
+        process.env.NODE_ENV === "production"
+            ? "combined"
+            : "dev"
+    )
+);
 app.use(express.json());
 
 app.use(rateLimiter);
