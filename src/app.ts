fb9d2eb27783601ebad85e5c35723cf676bb8bb6
komallsingh <singhkomal11711@gmail.com>
2026-07-20 13:16:22 +0530

    feat: updated the production middleware with compression

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import cors from "cors";
 import { rateLimiter } from "./middleware/rateLimiter";
 import helmet from "helmet";
 import morgan from "morgan";
+import compression from "compression";
+
 
 const app=express();
 app.use(cors());
@@ -18,6 +20,7 @@ app.use(
             : "dev"
     )
 );
+app.use(compression());
 app.use(express.json());
 
 app.use(rateLimiter);
