b78038022266be6f95b5bb4aaea725992ed688d8
komallsingh <singhkomal11711@gmail.com>
2026-07-20 13:03:51 +0530

    feat: add global rate limiting middleware

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import urlRoutes from "./routes/url.routes";
 import { errorHandler } from "./middleware/errorHandler";
-import e from "express";
 import authRoutes from "./routes/auth.routes";
 import cors from "cors";
+import { rateLimiter } from "./middleware/rateLimiter";
 
 const app=express();
 app.use(cors());
 app.use(express.json());
+
+app.use(rateLimiter);
 
 app.use("/api/auth",authRoutes);
 app.use("/api/url",urlRoutes);
---
+++ src/middleware/rateLimiter.ts
@@ -0,0 +1,14 @@
+import {rateLimit} from "express-rate-limit";
+import { success } from "zod";
+
+export const rateLimiter= rateLimit({
+    windowMs: 15*60*1000, //15 mins
+    limit: 5, //max 100 req per IP
+    standardHeaders: true,
+    legacyHeaders: false,
+    message:{
+        success: false,
+        message: "Too many requests, Please try again after sometime."
+    }
+});
+
