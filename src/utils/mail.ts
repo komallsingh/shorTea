42f62b137ea459f6ce4d1080ae2ee22e8650c945
komallsingh <singhkomal11711@gmail.com>
2026-07-24 15:21:57 +0530

    feat(feedback): fixing smtp failure bug - 3

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,11 +1,15 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
     },
+    connectionTimeout: 10000,
+    socketTimeout: 10000,
 });
 
 export const sendBugReport = async (
