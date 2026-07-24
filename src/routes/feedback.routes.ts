4fb7ae20c3a10caf8ebd2c89e574e69a871a76dc
komallsingh <singhkomal11711@gmail.com>
2026-07-24 13:54:00 +0530

    feat(feedback): fixed middleware bug

--- src/routes/feedback.routes.ts
+++ src/routes/feedback.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import * as feedbackController from "../controllers/feedback.controller";
+import { authMiddleware } from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.post(
     "/report-bug",
+    authMiddleware,
     feedbackController.reportBug
 );
 
